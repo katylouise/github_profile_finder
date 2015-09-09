71e69bbffab65e1381b62b9b3ed2c4a97d213a96
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-09 14:13:21 +0100

    Tested messages show on screen with protractor

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,7 +1,8 @@
 describe('Github Profile finder', function() {
   var searchBox = element(by.model('searchCtrl.searchTerm'));
   var searchButton = element(by.className('btn'));
-  var searchedFor = element(by.)
+  var searchedFor = element(by.className('searched_for'));
+  var searchResults = element(by.className('no_results'));
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
@@ -11,21 +12,15 @@ describe('Github Profile finder', function() {
     expect(browser.getTitle()).toEqual('Github user search');
   });
 
-  it('finds profiles', function() {
+  it('finds profiles and shows the search term', function() {
     searchBox.sendKeys('spike');
     searchButton.click();
 
     var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(searchedFor.getText()).toEqual("Search results for spike");
     expect(profiles.get(1).getText()).toEqual('spike01');
   });
 
-    it('finds the last Spike', function() {
-    searchBox.sendKeys('spike');
-    searchButton.click();
-
-    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
-    expect(profiles.last().getText()).toEqual('SpikeTheMaster');
-  });
 
   it('can count the number of spikes', function() {
     searchBox.sendKeys('spike');
@@ -35,12 +30,25 @@ describe('Github Profile finder', function() {
     });
   });
 
-  it('shows the search term', function() {
-    searchBox.sendKeys('spike');
+  it('shows no results when no users found', function() {
+    searchBox.sendKeys('87398jflijf87938');
     searchButton.click();
-    expect()
+    expect(searchResults.getText()).toEqual("No results found.");
+    element.all(by.repeater('user in searchCtrl.searchResult.items')).then(function(items) {
+      expect(items.length).toBe(0);
+    });
   });
+
   //   var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
   //   expect(profiles.length).toBe(20);
+  // });
+
+
+  //   it('finds the last Spike', function() {
+  //   searchBox.sendKeys('spike');
+  //   searchButton.click();
+
+  //   var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+  //   expect(profiles.last().getText()).toEqual('SpikeTheMaster');
   // });
 });
